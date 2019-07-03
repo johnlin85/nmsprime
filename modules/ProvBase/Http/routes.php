4cd01524e4b88b35714394674ee529b9a12c21f0
Patrick Reichel <patrick.reichel@roetzer-engineering.com>
2019-07-03 18:00:43 +0200

    Implement DocumentTemplate.render.
    This includes collecting data and testing templates.

--- modules/ProvBase/Http/routes.php
+++ modules/ProvBase/Http/routes.php
@@ -9,6 +9,7 @@ BaseRoute::group([], function () {
     BaseRoute::resource('Qos', 'Modules\ProvBase\Http\Controllers\QosController');
     BaseRoute::resource('Contract', 'Modules\ProvBase\Http\Controllers\ContractController');
     BaseRoute::resource('Domain', 'Modules\ProvBase\Http\Controllers\DomainController');
+    /* BaseRoute::resource('Document', 'Modules\ProvBase\Http\Controllers\DocumentController'); */
     BaseRoute::resource('DocumentTemplate', 'Modules\ProvBase\Http\Controllers\DocumentTemplateController');
     BaseRoute::resource('ProvBase', 'Modules\ProvBase\Http\Controllers\ProvBaseController');
 
@@ -25,4 +26,24 @@ BaseRoute::group([], function () {
             'middleware' => ['api', 'auth.basic', 'can:update,Modules\ProvBase\Entities\Modem'],
         ]);
     });
+
+    BaseRoute::get('DocumentTemplate/render/{template_id}/{model_id}', [
+        'as' => 'DocumentTemplate.render',
+        'uses' => 'Modules\ProvBase\Http\Controllers\DocumentTemplateController@render',
+        'middleware' => [
+            'can:view,Modules\ProvBase\Entities\DocumentTemplate',
+            'can:create,Modules\ProvBase\Entities\Document',
+            'can:update,Modules\ProvBase\Entities\Document',
+        ],
+    ]);
+
+    BaseRoute::get('DocumentTemplate/download_pdf/{b64_file}', [
+        'as' => 'DocumentTemplate.download_pdf',
+        'uses' => 'Modules\ProvBase\Http\Controllers\DocumentTemplateController@download_pdf',
+        'middleware' => [
+            'can:view,Modules\ProvBase\Entities\DocumentTemplate',
+            'can:create,Modules\ProvBase\Entities\Document',
+            'can:update,Modules\ProvBase\Entities\Document',
+        ],
+    ]);
 });
